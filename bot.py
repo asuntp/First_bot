3f4e4bbbfdb9560f2a3d66f8464ac9259bcafc6d
asuntp <asuntp81@gmail.com>
2019-11-30 14:25:54 +0300

    not work

--- bot.py
+++ bot.py
@@ -14,7 +14,6 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 # Функция greet_user будет вызаваться, когда пользователь напишет /start или подключится к боту в первый раз
 #Параметря принято называть bot & update (должно быть как минимум 2 параметра)
 def greet_user(bot, update):
-    #bot.send_message(chat_id=update.message.chat_id, text='Hi!')
     text = 'Call /start'
     print(text)
     logging.info(text)
@@ -24,8 +23,9 @@ def greet_user(bot, update):
 
 def talk_to_me(bot, update):
     user_text = 'Hello {}! You wrote: {}'.format(update.message.chat.username, update.message.text)
-    #bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
     print(update.message)
+    logging.info("User: %s, Chat id: %s, Message: %s", update.message.chat.username,
+                 update.message.chat.id, update.message.text)
     # эхобот ---отправляет пользователю тот текст который он написал
     update.message.reply_text(user_text)
 
@@ -35,16 +35,16 @@ def error(bot, update):
 
 # Функция которая соединяется с платформой telegram
 def main():
-    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=settings.PROXY)
+    updater = Updater(settings.API_KEY, use_context=True, request_kwargs=settings.PROXY)
     logging.info('bot start')
 
-    dp = mybot.dispatcher
+    dp = updater.dispatcher
     dp.add_handler(CommandHandler('start', greet_user)) #Когда придет команда start выполнится функция
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     dp.add_error_handler(error)
 
-    mybot.start_polling()
-    mybot.idle()
+    updater.start_polling()
+    updater.idle()
 
 main()
 
